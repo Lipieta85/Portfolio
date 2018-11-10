1a8ca711e40a799c683da65865f706a65281cb29
Rafał Lipieta <cwgflojdid@wp.pl>
2018-11-10 18:14:08 +0100

    Update

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    var templateList = document.getElementById('template-product-list').innerHTML;
+
+    Mustache.parse(templateList);
+
+
+    for (var i = 0; i < productsData.length; i++) {
+        document.querySelector('.main-carousel').innerHTML += Mustache.render(templateList, productsData[i])
+    }
+
+
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity(elem, {
+
+        hash: true,
+        pageDots: false,
+        cellAlign: 'center',
+        contain: true
+    });
+
+    var $carousel = $('.main-carousel').flickity();
+    var $progressBar = $('.progress-bar');
+
+    $carousel.on('scroll.flickity', function (event, progress) {
+        progress = Math.max(0, Math.min(1, progress));
+        $progressBar.width(progress * 100 + '%');
+    });
+
+    var carouselData = Flickity.data($carousel[0])
+    var progress = (carouselData.selectedIndex + 1) / carouselData.cells.length
+    $progressBar.width(progress * 100 + '%');
+
+    $('.button-group').on('click', '.button', function () {
+        var index = $(this).index();
+        $carousel.flickity('selectCell', index);
+    });
+});
